af18a6f8434480153933547cd4cb5dbb65e2bafe
Seok <tjr50999@naver.com>
2021-02-20 06:01:03 +0900

    Update 01

--- JS/Todo/Basic01/main.js
+++ JS/Todo/Basic01/main.js
@@ -3,11 +3,12 @@ const todoList = document.querySelector(".todo-list");
 
 const handleKeypress = function (e) {     // 여기서 e는 기본적으로 제공되는 객체인데 input event가 발생했을 때 발생한 event의 정보를 담고있는 객체이다.
   if (e.keyCode === 13) {
-    console.log(todoInput.value)
-    const li = document.createElement("li");
-    li.innerText = todoInput.value
-    todoList.appendChild(li)
+    generateTodo(todoInput.value)
   }
+}
+
+const generateTodo = (todo) => {
+  const li = document.createElement("li");
 }
 
 todoInput.addEventListener("keypress", handleKeypress)
